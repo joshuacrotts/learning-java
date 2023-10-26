aad556965f2eb12a41e0b8c4a5cf8b474e73cd25
Joshua Crotts <joshuacrotts@outlook.com>
2023-10-26 15:54:12 -0400

    More about interfaces.

---
+++ fix-repo.sh
@@ -0,0 +1,9 @@
+cd .. \
+&& rm -rf new-java \
+&& mkdir new-java \
+&& cd new-java \
+&& git clone git@github.com:joshuacrotts/testing-java.git \
+&& cd .. \
+&& cd testing-java \
+&& rm -rf *.git \
+&& cp -r ../new-java/testing-java/.git .
