d8aaa3e758fddd0958ee5fe1d18757d4cd746d4d
Joshua Crotts <joshuacrotts@outlook.com>
2024-05-15 20:27:12 -0400

    Added new appendix.

--- res/java-code/TeachingJava/src/teachingjava/trees/BinarySearchTree.java
+++ res/java-code/TeachingJava/src/teachingjava/trees/BinarySearchTree.java
@@ -1,28 +1,102 @@
 package teachingjava.trees;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Comparator;
 
+/**
+ * This class represents a binary search tree.
+ * The tree is ordered according to the natural order of the elements or a custom comparator.
+ * The tree does not allow duplicate elements.
+ * @param <T> the type of elements stored in the tree. Whatever type is used must implement Comparable.
+ */
 class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
 
+  /**
+   * Data associated with this node.
+   */
+  private final T DATA;
+
+  /**
+   * Comparator instance used to order the elements.
+   * If no comparator is passed, the natural order of the elements is used.
+   */
+  private final Comparator<T> COMPARATOR;
+
+  /**
+   * Left child of this node, or null if it doesn't exist.
+   */
   private BinarySearchTree<T> left;
+
+  /**
+   * Right child of this node, or null if it doesn't exist.
+   */
   private BinarySearchTree<T> right;
+
+  /**
+   * Parent of this node.
+   */
   private BinarySearchTree<T> parent;
-  private T data;
 
-  private Comparator<T> comparator;
-
-  BinarySearchTree(T data) {
+  BinarySearchTree(@NotNull T data) {
     this(data, Comparator.naturalOrder());
   }
 
-  BinarySearchTree(T data, Comparator<T> comparator) {
-    this.data = data;
-    this.comparator = comparator;
+  BinarySearchTree(@NotNull T data, @NotNull Comparator<T> comparator) {
+    this.DATA = data;
+    this.COMPARATOR = comparator;
     this.left = this.right = this.parent = null;
   }
 
+
+  /**
+   * Inserts a new element into this binary search tree.
+   * The element cannot be null nor can it be a duplicate of an existing element.
+   * - If the element is less than the current node, it is inserted into the left subtree.
+   * - If the element is greater than the current node, it is inserted into the right subtree.
+   * @param data the element to insert.
+   */
+  @Override
+  public void insert(@NotNull T data) {
+    if (this.COMPARATOR.compare(data, this.DATA) < 0) {
+      if (this.left == null) {
+        this.left = new BinarySearchTree<>(data, this.COMPARATOR);
+        this.left.parent = this;
+      } else {
+        this.left.insert(data);
+      }
+    } else {
+      if (this.right == null) {
+        this.right = new BinarySearchTree<>(data, this.COMPARATOR);
+        this.right.parent = this;
+      } else {
+        this.right.insert(data);
+      }
+    }
+  }
+
+  @Override
+  public @Nullable Tree<T> contains(@NotNull T data) {
+    if (this.COMPARATOR.compare(data, this.DATA) == 0) {
+      return this;
+    } else if (this.COMPARATOR.compare(data, this.DATA) < 0) {
+      return this.left == null ? null : this.left.contains(data);
+    } else {
+      return this.right == null ? null : this.right.contains(data);
+    }
+  }
+
+  @Override
+  public String toString() {
+    // Return an in-order traversal of the tree.
+    return (this.left == null ? "" : this.left + ", ")
+            + this.DATA
+            + (this.right == null ? "" : ", " + this.right);
+  }
+
   T getData() {
-    return this.data;
+    return this.DATA;
   }
 
   BinarySearchTree<T> getLeft() {
@@ -43,41 +117,5 @@ class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
 
   boolean isRoot() {
     return this.parent == null;
-  }
-
-  @Override
-  public void insert(T data) {
-    if (this.comparator.compare(data, this.data) < 0) {
-      if (this.left == null) {
-        this.left = new BinarySearchTree<>(data, this.comparator);
-        this.left.parent = this;
-      } else {
-        this.left.insert(data);
-      }
-    } else {
-      if (this.right == null) {
-        this.right = new BinarySearchTree<>(data, this.comparator);
-        this.right.parent = this;
-      } else {
-        this.right.insert(data);
-      }
-    }
-  }
-
-  @Override
-  public Tree<T> contains(T data) {
-    if (this.comparator.compare(data, this.data) == 0) {
-      return this;
-    } else if (this.comparator.compare(data, this.data) < 0) {
-      return this.left == null ? null : this.left.contains(data);
-    } else {
-      return this.right == null ? null : this.right.contains(data);
-    }
-  }
-
-  @Override
-  public String toString() {
-    // Return an in-order traversal of the tree.
-    return (this.left == null ? "" : this.left.toString() + ", ") + this.data + (this.right == null ? "" : ", " + this.right.toString());
   }
 }
--- res/java-code/TeachingJava/src/teachingjava/trees/Tree.java
+++ res/java-code/TeachingJava/src/teachingjava/trees/Tree.java
@@ -1,5 +1,9 @@
 package teachingjava.trees;
 
+/**
+ * This interface represents any possible tree data structure that supports insertion and search.
+ * @param <T> the type of elements stored in the tree. Whatever type is used must implement Comparable.
+ */
 interface Tree<T extends Comparable<T>> {
 
   void insert(T data);
